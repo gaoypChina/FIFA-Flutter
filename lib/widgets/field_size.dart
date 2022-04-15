7397e28c3a033fb803d66ad7b21089268c7c043e
Marcos14Almeida <marcos.10palmeida@gmail.com>
2022-04-15 15:20:25 -0300

    Commit 6ªfeira 15-04 files

---
+++ lib/widgets/field_size.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+Widget fieldSizeWidget(Widget childrenWidget){
+  return SizedBox(
+    height: 330,
+    child: Stack(
+      children: [
+        //Campo
+        Image.asset('assets/icons/campo metade.png',height:double.infinity,width:double.infinity,fit:BoxFit.fill),
+
+        childrenWidget,
+      ],
+    ),
+  );
+}
